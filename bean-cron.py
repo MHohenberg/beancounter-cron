ba2f27fd9cebb922fe74352cb8133f5b82f16414
Martin Hohenberg <martin.hohenberg@gmail.com>
2018-12-28 16:24:15 +0100

    gitignore, License

--- bean-cron.py
+++ bean-cron.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python
+
+# beancount-cron - automatically add automatic payments to a beancount file
+# Copyright (C) 2018 Martin Hohenberg <me@martinhohenberg.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+def getNextDate(startDate, designator):
+
+    endDate = startDate
+    designator = designator.lower()
+    multiplier = 1
+    if (designator[0].isDigit()):
+        multiplier = int(designator[0])
+        designator = designator[1,]
+
+    if (designator == "yearly"):
+        
+        
+    if (designator == "monthly"):
+        
+        
+    if (designator == "weekly"):
+        days = 7*multiplier
+        endDate = startDate.addDays(days)
+
+    if (designator == "daily"):
+        days = 1*multiplier
+        endDate = startDate.addDays(days)
+
+# open the file
+
+# do the goddamn stuff
+
